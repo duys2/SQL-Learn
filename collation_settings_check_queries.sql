5922288ea77d3362edbf05c4229652b3f26fd60e
duys2 <115527199+duys2@users.noreply.github.com>
2024-08-07 01:01:12 +0900

    [Chore] Update collation_settings_check_queries.sql

--- collation_settings_check_queries.sql
+++ collation_settings_check_queries.sql
@@ -3,11 +3,11 @@
 # 데이터베이스의 정렬 설정 확인
 SELECT SCHEMA_NAME, DEFAULT_COLLATION_NAME 
 FROM information_schema.SCHEMATA 
-WHERE SCHEMA_NAME = 'movieblog'; -- ''안 DB명
+WHERE SCHEMA_NAME = 'DataBaseName'; -- ''안 DB명
 
 # 테이블의 정렬 설정 확인
 SHOW TABLE STATUS 
-WHERE Name = 'post'; -- ''안 테이블명 입력
+WHERE Name = 'TableName'; -- ''안 테이블명 입력
 
 # 컬럼의 정렬 설정 확인
-SHOW FULL COLUMNS FROM post; -- 테이블명 입력
+SHOW FULL COLUMNS FROM TableName; -- 테이블명 입력
